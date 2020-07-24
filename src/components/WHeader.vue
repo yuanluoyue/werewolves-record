0e84688ab6a550e6764d5e751e37a524904dd632
noname <yeunghuanyuan@qq.com>
2020-07-24 17:05:34 +0800

    add header

---
+++ src/components/WHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="4">
+      <div class="grid-content bg-purple">0</div>
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content bg-purple">0</div>
+    </el-col>
+    <el-col :offset="8" :span="8">
+      <div class="grid-content bg-purple-light">
+        <el-select v-model="value" placeholder="人数">
+          <el-option
+            v-for="item in numOfPlayerSelectOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+      numOfPlayerSelectOptions: [],
+      value: ''
+    }
+  },
+
+  mounted() {
+    this.initPlayerSelectOptions()
+  },
+
+  methods: {
+    initPlayerSelectOptions() {
+      const minPlayer = 6
+      const maxPlayer = 18
+      const options = []
+      for (let i = 0; i < maxPlayer - minPlayer + 1; i++) {
+        const n = minPlayer + i
+        const o = {
+          value: n,
+          label: n + '人',
+        }
+        options.push(o)
+      }
+      this.numOfPlayerSelectOptions = options
+    }
+  }
+}
+</script>
+  
+<style>
+</style>
+
---
+++ src/page/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container>
+    <el-header>
+      <W-header />
+    </el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<script>
+import WHeader from '../components/WHeader'
+export default {
+  name: 'HomePage',
+  components: {
+    WHeader
+  }
+}
+</script>
+  
+<style>
+</style>
+
